cc234e0eab4a88b68358a0ef9e8fad9ca4406433
NayanCod <kumarnayab6@gmail.com>
2025-03-09 13:20:52 +0530

    protected routes ahs been intialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import { useEffect, useState } from 'react';
+import NotFound from './components/NotFound';
 
 function App() {
-  
+  const [isAuthneticate, setIsAuthenticate] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if(token){
+      setIsAuthenticate(true);
+    }else{
+      setIsAuthenticate(false);
+    }
+  }, [])
+
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={<Login />} />
+                <Route path="/login" element={isAuthneticate ? <Navigate to="/dashboard" /> : <Login/>} />
                 
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={isAuthneticate ? <Dashboard /> : <Navigate to="/login" />} />
                 
-                <Route path="*" element={<Login />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
