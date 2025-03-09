1190813b8b2ae6502ba327b42c1fce1f4f682a49
NayanCod <kumarnayab6@gmail.com>
2025-03-09 13:36:39 +0530

    dashbaord page initliazes

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
-      dashboard
-    </div>
+    <>
+        <div className='h-screen'>
+            <div className='flex justify-between items-center pt-2'>
+                <h1 className='text-2xl text-gray-400'>Hello, <span className='text-4xl text-white'>Nayan</span></h1>
+                <div className='flex items-center gap-3'>
+                    <div className="avatar">
+                        <div className="ring-primary ring-offset-base-100 w-10 rounded-full">
+                            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
+                    <div className="avatar w-10">
+                        <img className='w-10 rounded-full' src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
